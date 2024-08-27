26710974de4b301cd6fff7105cdfd8b0af8cc60c
meersuhaib191 <meersuhaib119@gmail.com>
2024-08-27 14:53:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Tech Enthusiast.", "Tech Enthusiast.", "Tech Enthusiast."],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
