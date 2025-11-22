308e69a8ba82980f8398e7b1ee3260e0c270d0aa
meersuhaib191 <meersuhaib119@gmail.com>
2025-11-22 19:44:50 +0530

    updated

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings:["Tech Enthusiast.", "Tech Enthusiast.", "Tech Enthusiast."],
-    typeSpeed : 10,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
-});
